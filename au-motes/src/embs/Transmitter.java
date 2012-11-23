07f00be13fcb908bd3af52132b1fb399019c5a17
Matt Button <matthew@sigswitch.com>
2012-11-23 14:21:33 +0000

    Adding basic radio transmitter

--- au-motes/src/embs/Transmitter.java
+++ au-motes/src/embs/Transmitter.java
@@ -1,5 +1,20 @@
 package embs;
 
+import com.ibm.saguaro.system.Radio;
+
 public class Transmitter {
+
+	static private byte initialChannel = 11;
+	static private Radio radio = new Radio();
 	
+	static {
+		// Open the default radio
+        radio.open(Radio.DID, null, 0, 0);
+        
+        // Set channel 
+        radio.setChannel(initialChannel);
+        radio.setPanId(initialChannel, true);
+        radio.setShortAddr(0x14);
+        
+	}
 }
