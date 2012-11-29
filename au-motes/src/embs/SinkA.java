a74f04c2aaffd665c9ffd02fcf752e4b5cb16cf1
Matt Button <matthew@sigswitch.com>
2012-11-29 02:18:05 +0000

    Make sinks show packets received out of period

--- au-motes/src/embs/SinkA.java
+++ au-motes/src/embs/SinkA.java
@@ -5,161 +5,154 @@ import com.ibm.saguaro.logger.*;
 
 public class SinkA {
 
-    private static Timer tsend;
-    private static Timer tstart;
-    private static Timer rEnd;
-    
-    private static boolean light=true;
-    
-    private static byte[] xmit;
-    private static long   wait;
-    static Radio radio = new Radio();
-    private static int n = 6; // number of beacons of sync phase - sample only, assessment will use unknown values
-    private static int nc;
-    
-    private static int t = 800; // milliseconds between beacons - sample only, assessment will use unknown values 
-    
-    // settings for sink A
-    private static byte channel = 0; // channel 11
-    private static byte panid = 0x11;
-    private static byte address = 0x11;
+	private static Timer tsend;
+	private static Timer tstart;
+	private static Timer rEnd;
+
+	private static boolean light = true;
+
+	private static byte[] xmit;
+	private static long wait;
+	static Radio radio = new Radio();
+	private static int n = 5; // number of beacons of sync phase - sample only,
+								// assessment will use unknown values
+	private static int nc;
+
+	private static int t = 500; // milliseconds between beacons - sample only,
+								// assessment will use unknown values
+
+	// settings for sink A
+	private static byte channel = 0; // channel 11
+	private static byte panid = 0x11;
+	private static byte address = 0x11;
 	private static boolean inReceivePeriod;
 
-    static {
-        // Open the default radio
-        radio.open(Radio.DID, null, 0, 0);
+	static {
+		// Open the default radio
+		radio.open(Radio.DID, null, 0, 0);
 
-        radio.startRx(Device.ASAP, 0, Time.currentTicks() + 0x7FFFFFFF);
+		// Set channel
+		radio.setChannel((byte) channel);
 
-        // Set channel 
-        radio.setChannel((byte)channel);
+		// Set the PAN ID and the short address
+		radio.setPanId(panid, true);
+		radio.setShortAddr(address);
 
-        // Set the PAN ID and the short address
-        radio.setPanId(panid, true);
-        radio.setShortAddr(address);
+		radio.startRx(Device.ASAP, 0, Time.currentTicks() + 0x7FFFFFFF);
+		
+		// Prepare beacon frame with source and destination addressing
+		xmit = new byte[12];
+		xmit[0] = Radio.FCF_BEACON;
+		xmit[1] = Radio.FCA_SRC_SADDR | Radio.FCA_DST_SADDR;
+		Util.set16le(xmit, 3, panid); // destination PAN address
+		Util.set16le(xmit, 5, 0xFFFF); // broadcast address
+		Util.set16le(xmit, 7, panid); // own PAN address
+		Util.set16le(xmit, 9, address); // own short address
 
-        // Prepare beacon frame with source and destination addressing
-        xmit = new byte[12];
-        xmit[0] = Radio.FCF_BEACON;
-        xmit[1] = Radio.FCA_SRC_SADDR|Radio.FCA_DST_SADDR;
-        Util.set16le(xmit, 3, panid); // destination PAN address 
-        Util.set16le(xmit, 5, 0xFFFF); // broadcast address 
-        Util.set16le(xmit, 7, panid); // own PAN address 
-        Util.set16le(xmit, 9, address); // own short address 
-
-        xmit[11] = (byte)n;
+		xmit[11] = (byte) n;
 
 		// register delegate for received frames
-        radio.setRxHandler(new DevCallback(null){
-                public int invoke (int flags, byte[] data, int len, int info, long time) {
-                    return  SinkA.onReceive(flags, data, len, info, time);
-                }
-            });
+		radio.setRxHandler(new DevCallback(null) {
+			public int invoke(int flags, byte[] data, int len, int info,
+					long time) {
+				return SinkA.onReceive(flags, data, len, info, time);
+			}
+		});
+
+		// Setup a periodic timer callback for beacon transmissions
+		tsend = new Timer();
+		tsend.setCallback(new TimerEvent(null) {
+			public void invoke(byte param, long time) {
+				SinkA.periodicSend(param, time);
+			}
+		});
+
+		// Setup a periodic timer callback to restart the protocol
+		tstart = new Timer();
+		tstart.setCallback(new TimerEvent(null) {
+			public void invoke(byte param, long time) {
+				SinkA.restart(param, time);
+			}
+		});
+
+		// Timer to cancel reception period
+		rEnd = new Timer();
+		rEnd.setCallback(new TimerEvent(null) {
+			public void invoke(byte param, long time) {
+				SinkA.stopReceivePeriod(param, time);
+			}
+		});
 
 
-        
-        // Setup a periodic timer callback for beacon transmissions
-        tsend = new Timer();
-        tsend.setCallback(new TimerEvent(null){
-                public void invoke(byte param, long time){
-                    SinkA.periodicSend(param, time);
-                } 
-            });
-            
-            
-        
-        
-        // Setup a periodic timer callback to restart the protocol
-        tstart = new Timer();
-        tstart.setCallback(new TimerEvent(null){
-                public void invoke(byte param, long time){
-                    SinkA.restart(param, time);
-                }
-            });
-        
-        rEnd.setCallback(new TimerEvent(null){
-        		public void invoke(byte param, long time){
-        			SinkA.stopReceivePeriod(param, time);
-        		}
-        });
+		// Convert the periodic delay from ms to platform ticks
+		wait = Time.toTickSpan(Time.MILLISECS, t);
 
-            
-        // Convert the periodic delay from ms to platform ticks
-        wait = Time.toTickSpan(Time.MILLISECS, t);
-        
-        
-        tstart.setAlarmBySpan(Time.toTickSpan(Time.SECONDS, 5)); //starts the protocol 5 seconds after constructing the assembly
-    }
+		// starts the protocol 5 seconds after constructing the assembly
+		tstart.setAlarmBySpan(Time.toTickSpan(Time.SECONDS, 5)); 
+	}
 
-    // Called when a frame is received or at the end of a reception period 
-    private static int onReceive (int flags, byte[] data, int len, int info, long time) {
-        if (data == null) {
-        	radio.startRx(Device.ASAP, 0, Time.currentTicks() + 0x7FFFFFFF);
-            
-                    
-            return 0;
-        }
+	// Called when a frame is received
+	private static int onReceive(int flags, byte[] data, int len, int info,
+			long time) {
+		if (data == null) {
+			radio.startRx(Device.ASAP, 0, Time.currentTicks() + 0x7FFFFFFF);
 
+			return 0;
+		}
+
+		// Red if in receive period, else yellow
+		byte led = (byte) (inReceivePeriod ? 2 : 0);
 
 		// frame received, so blink red LED and log its payload
 
-        if(light){
-         	LED.setState((byte)2, (byte)1);
-        }
-        else{
-        	LED.setState((byte)2, (byte)0);
+		LED.setState(led, (byte) (LED.getState(led) == 0 ? 1 : 0));
+
+		if (inReceivePeriod) {
+			Logger.appendString(csr.s2b("In Reception: "));
+		} else {
+			Logger.appendString(csr.s2b("NOT In Reception: "));
 		}
-		light=!light;
-		
+
 		Logger.appendByte(data[11]);
-        Logger.flush(Mote.WARN);
-        return 0;
-        
-    }
+		Logger.flush(Mote.WARN);
+		return 0;
 
-
-    protected static void stopReceivePeriod(byte param, long time) {
-		inReceivePeriod = false;
-		
-		// turn green LED off 
-        LED.setState((byte)1, (byte)0);
-        
-        //set alarm to restart protocol
-    	tstart.setAlarmBySpan(10*wait);
 	}
 
+	protected static void stopReceivePeriod(byte param, long time) {
+		inReceivePeriod = false;
+
+		// turn green LED off
+		LED.setState((byte) 1, (byte) 0);
+
+		// set alarm to restart protocol
+		tstart.setAlarmBySpan(10 * wait);
+	}
 
 	// Called on a timer alarm
-    public static void periodicSend(byte param, long time) {
-        
-        if(nc>0){
-	        // transmit a beacon 
-    	    radio.transmit(Device.ASAP|Radio.TXMODE_POWER_MAX, xmit, 0, 12, 0);
-        	// program new alarm
-        	tsend.setAlarmBySpan(wait);
-        	nc--;
-        	xmit[11]--;
-        }
-        else{
-        	//start reception phase
-	        
-	        // turn green LED on 
-	        LED.setState((byte)1, (byte)1);
-	        inReceivePeriod = true;
-	        rEnd.setAlarmTime(time + wait);
-        }
-    }
+	public static void periodicSend(byte param, long time) {
 
+		if (nc > 0) {
+			// transmit a beacon
+			radio.transmit(Device.ASAP | Radio.TXMODE_POWER_MAX, xmit, 0, 12, 0);
+			// program new alarm
+			tsend.setAlarmBySpan(wait);
+			nc--;
+			xmit[11]--;
+		} else {
+			// turn green LED on
+			LED.setState((byte) 1, (byte) 1);
+			inReceivePeriod = true;
+			rEnd.setAlarmBySpan(wait);
+		}
+	}
 
-    // Called on a timer alarm, starts the protocol
-    public static void restart(byte param, long time) {
-        
-        nc=n;
-        xmit[11]=(byte)n;
-       	tsend.setAlarmBySpan(0);
-        
-    }
+	// Called on a timer alarm, starts the protocol
+	public static void restart(byte param, long time) {
+		nc = n;
+		xmit[11] = (byte) n;
+		tsend.setAlarmBySpan(0);
 
-
+	}
 
 }
